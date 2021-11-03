112a952e68140948ac0510852ab302edf85f40c3
Michelle Bodart <michelle.bodart@gmail.com>
2021-11-03 16:17:52 -0700

    not sure why I need to commit this again

--- app/models/author.py
+++ app/models/author.py
@@ -3,8 +3,4 @@ from app import db
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-<<<<<<< HEAD
-    books = db.relationship("book", back_populates="author")
-=======
     books = db.relationship("Book", back_populates="author")
->>>>>>> working_branch
--- app/models/book.py
+++ app/models/book.py
@@ -4,9 +4,5 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-<<<<<<< HEAD
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-=======
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
->>>>>>> working_branch
     author = db.relationship("Author", back_populates="books")
